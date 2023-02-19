89389ff770bef4d1a02a10aaf1400e72b2edfe68
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-02-19 15:51:03 +0200

    finished homeWork

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+const inputEl = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => createBoxes(inputEl.value));
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 30;
+  const elements = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${size}px`;
+    divEl.style.height = `${size}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    elements.push(divEl);
+    size += 10;
+  }
+
+  boxesEl.append(...elements);
 }
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// Цей скрипт зчитує значення з input, яке користувач вводить, 
+// і за його допомогою викликає функцію createBoxes, 
+// яка створює колекцію елементів div в div#boxes. 
+// Розмір кожного елементу збільшується на 10px в порівнянні 
+// з попереднім елементом. Колір фону елементів випадково 
+// генерується за допомогою функції getRandomHexColor.
+
+// Функція destroyBoxes очищає колекцію елементів div#boxes.
