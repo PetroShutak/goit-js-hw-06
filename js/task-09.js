7be4115965270e82e1883a62c825ac1d32d45405
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-02-19 16:29:40 +0200

    conflict id (maybe)

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,5 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777216).toString(16)}`;
 }
 const colorSpan = document.querySelector('.color');
 const changeColorButton = document.querySelector('.change-color');
